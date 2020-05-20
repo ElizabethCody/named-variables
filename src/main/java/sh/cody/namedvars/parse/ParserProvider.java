2dcafb54a9b60073c70c15f3e8277a83946213c4
Maxwell Cody <maxwell@cody.sh>
2020-05-20 05:12:14 -0500

    [parser] Added initial parser interface.

---
+++ src/main/java/sh/cody/namedvars/parse/ParserProvider.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 - Maxwell Cody <maxwell@cody.sh>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sh.cody.namedvars.parse;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+public abstract class ParserProvider {
+   public abstract <T> Parser<T> match(Class<T> type);
+
+   protected static <T> Parser<T> matcher(Class<T> type, Function<Class, Parser>... functions) {
+      for(Function<Class, Parser> function : functions) {
+         Parser<T> parser = function.apply(type);
+         if(parser != null) {
+            return parser;
+         }
+      }
+
+      return null;
+   }
+
+   protected static <T> Function<Class, Parser> parserFor(Class<T> parserType, Parser<T> parser) {
+      return type -> Optional.of(type).filter(parserType::equals).map(ignored -> parser).orElse(null);
+   }
+}
---
+++ src/main/java/sh/cody/namedvars/parse/SimpleParserProvider.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020 - Maxwell Cody <maxwell@cody.sh>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package sh.cody.namedvars.parse;
+
+@SuppressWarnings("unchecked")
+public final class SimpleParserProvider extends ParserProvider {
+   @Override
+   public <T> Parser<T> match(Class<T> type) {
+      return matcher(type,
+            parserFor(boolean.class, Boolean::parseBoolean),
+            parserFor(byte.class, Byte::parseByte),
+            parserFor(short.class, Short::parseShort),
+            parserFor(char.class, string -> string.charAt(0)),
+            parserFor(int.class, Integer::parseInt),
+            parserFor(long.class, Long::parseLong),
+            parserFor(float.class, Float::parseFloat),
+            parserFor(double.class, Double::parseDouble),
+            parserFor(Boolean.class, Parser.nullChecked(Boolean::parseBoolean)),
+            parserFor(Byte.class, Parser.nullChecked(Byte::parseByte)),
+            parserFor(Short.class, Parser.nullChecked(Short::parseShort)),
+            parserFor(Character.class, str -> (str != null && str.length() > 0) ? str.charAt(0) : null),
+            parserFor(Integer.class, Parser.nullChecked(Integer::parseInt)),
+            parserFor(Long.class, Parser.nullChecked(Long::parseLong)),
+            parserFor(Float.class, Parser.nullChecked(Float::parseFloat)),
+            parserFor(Double.class, Parser.nullChecked(Double::parseDouble))
+      );
+   }
+}
